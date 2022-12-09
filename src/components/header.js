4c7877256d77bc149e906b9524eb61688fcdb89f
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-09 15:34:41 -0600

    header style updated, near final

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,12 @@ export default function Header() {
     return (
 
         <div className="header">
-            <h3>Chuck Gibson</h3><h3> - Web Developer</h3>
+            <h3><span>C</span>huck <span>G</span>ibson</h3><h3> - Web Developer</h3>
             <ul>
-               <li>About</li>
-               <li>Skills</li>
-               <li>Projects</li>
-               <li>Contact</li> 
+               <li><a href="/" data-content="About"><span>A</span>bout</a></li>
+               <li><a href="/"  data-content="Skills"><span>S</span>kills</a></li>
+               <li><a href="/"  data-content="Projects"><span>P</span>rojects</a></li>
+               <li><a href="/"  data-content="Contact"><span>C</span>ontact</a></li> 
             </ul>
         </div>
     )

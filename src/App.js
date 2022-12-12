7fe309ca3185ff84ce79dc67d20025e9cd63eee0
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-12 17:06:45 -0600

    updated style to variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Router,Routes} from 'react-router';
+//import {Router,Routes} from 'react-router';
 import Header from "./components/header";
 
 function App() {

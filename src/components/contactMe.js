904a3c9211bbcf897e43afe5663dc0c77df3b9b2
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-15 17:20:09 -0600

    cleaned skills, added smooth scroll

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -3,8 +3,8 @@ import "./contactMe.css";
 
 export default function ContacMe(){
     return (
-        <div className="contactMe-container">
-            <h2 id="contact"> Have any <span>Questions?</span></h2>
+        <div id="contact" className="contactMe-container">
+            <h2 > Have any <span>Questions?</span></h2>
             <p> If my site has peaked your interest, feel free to get in touch for buisness inquiries, resume requests or any further question you may have, and I'll get back to you asap</p>
             <a href="mailto:chuckgibson@live.ca">Contact Me</a>
         </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -3,10 +3,10 @@ import "./skills.css";
 
 export default function Skills() {
      return (
-        <div className="skills-container">
+        <div className="skills-container" id="skills">
             <h2><hr/><span>S</span>kills<hr/></h2>
             <div className='skills'>
-                <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"><i className="fab fa-html5"></i><p>Html</p></a></div>
+                <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" ><i className="fab fa-html5"></i><p>Html</p></a></div>
                 <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><i className="fab fa-css3-alt"></i><p>Css</p></a></div>
                 <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><i className="fab fa-js"></i><p>JavaScript</p></a></div>
                 <div classname="skill"><a href="https://github.com"><i className="fab fa-github"></i><p>Github</p></a></div>

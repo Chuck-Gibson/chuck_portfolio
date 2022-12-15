f3922d82822c285584d60097b968e2466f08e637
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-15 17:02:53 -0600

    contact me finished

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./contactMe.css";
 
 export default function ContacMe(){
     return (
         <div className="contactMe-container">
-            <h2 id="contact">Lets get in touch</h2>
+            <h2 id="contact"> Have any <span>Questions?</span></h2>
+            <p> If my site has peaked your interest, feel free to get in touch for buisness inquiries, resume requests or any further question you may have, and I'll get back to you asap</p>
+            <a href="mailto:chuckgibson@live.ca">Contact Me</a>
         </div>
     )
 }

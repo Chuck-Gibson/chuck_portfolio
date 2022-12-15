4bc75b5e118a05147e862703429daa2e484428f3
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-15 16:22:29 -0600

    styled skills and added links

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,14 +6,14 @@ export default function Skills() {
         <div id="skills">
             <h2><hr/><span>S</span>kills<hr/></h2>
             <div className='skills-container'>
-                <i className="fab fa-html5"></i>
-                <i className="fab fa-css3-alt"></i>
-                <i className="fab fa-js"></i>
-                <i className="fab fa-github"></i>
-                <i className="fab fa-git-alt"></i>
-                <i className="fab fa-chrome"></i>
-                <i className="fab fa-react"></i>
-                <i className="fab fa-node-js"></i>
+                <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"><i className="fab fa-html5"></i><p>Html</p></a></div>
+                <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><i className="fab fa-css3-alt"></i><p>Css</p></a></div>
+                <div classname="skill"><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><i className="fab fa-js"></i><p>JavaScript</p></a></div>
+                <div classname="skill"><a href="https://github.com"><i className="fab fa-github"></i><p>Github</p></a></div>
+                <div classname="skill"><a href="https://git-scm.com"><i className="fab fa-git-alt"></i><p>Git</p></a></div>
+                <div classname="skill"><a href="https://developer.chrome.com/docs/devtools"><i className="fab fa-chrome"></i><p>Chrome Dev</p></a></div>
+                <div classname="skill"><a href="https://reactjs.org"><i className="fab fa-react"></i><p>React</p></a></div>
+                <div classname="skill"><a href="https://nodejs.org/en/about/"><i className="fab fa-node-js"></i><p>Node</p></a></div>
             </div>
         </div>
      )

3132dd44fd04e3354dc1380134d8fe2aca12df04
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-12-20 17:31:33 -0600

    finished off responseive for mobile

--- src/components/about.js
+++ src/components/about.js
@@ -8,14 +8,10 @@ export default function About() {
             <div className='content-container'>
             <img src={require("../resources/images/cartoonchuck.png")} alt="a cartoon of Chuck Gibson"/>
             <div className="text-container">
-            
-            <p>
-
-I am from canada and am an aspiring coder.</p> <p>Forklift driver by trade I've always been interested in how things work, none more so than computers and computer programs.</p><p>
-
-After discovery of a new open-source streaming platform called <span>Glimesh</span> and learning that i could help build and grow the site if i had some technical coding skills I was inspired to begin my.</p><p>
-
-My current skills touch primarily on the Front end side of websites but I'm excited to be learning more Back end and Dev ops as continue my studies</p>
+                <p>I am from canada and am an aspiring coder.</p> 
+                <p>Forklift driver by trade I've always been interested in how things work, none more so than computers and computer programs.</p>
+                <p>After discovery of a new open-source streaming platform called <span>Glimesh</span> and learning that i could help build and grow the site if i had some technical coding skills I was inspired to begin my learning.</p>
+                <p>My current skills touch primarily on the Front end side of websites but I'm excited to be learning more Back end and Dev ops as continue my studies</p>
             </div>
             </div>
         </div>

afd4bf18f2db55c78829798541b5c84cdd1eb43b
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2023-01-05 17:24:41 -0600

    fixed grammar in intro

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,7 @@ export default function Intro() {
             <div className="intro">
             <p><span>Hi my name is...</span></p>
             <h1 id="name">Chuck Gibson</h1><h1>and i build things for <span>the web</span></h1>
-            <p>I am a self taught Full stack developer, and it is my goal to build apps and software that people would love to use </p>
+            <p>I am a self-taught Full stack developer, and it is my goal to build apps and software that people would love to use </p>
             </div>
         </div>
     )
